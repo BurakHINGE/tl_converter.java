da5602f4d5435ea9ad3b711efeee87b901472fc7
BurakHINGE <burakmentese16@gmail.com>
2025-12-27 22:06:41 +0300

    Bug
    Bug has been fixed.

--- eng_dollar_conventer.java
+++ eng_dollar_conventer.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class USDConverter {
+public class eng_dollar_conventer {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
