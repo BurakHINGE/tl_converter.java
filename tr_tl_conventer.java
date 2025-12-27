dea096575b7fbb97e400a46de44b7e566f54931c
BurakHINGE <burakmentese16@gmail.com>
2025-12-27 22:07:09 +0300

    Bug
    Bug has been fixed in Turkish version.

--- tr_tl_conventer.java
+++ tr_tl_conventer.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TLConverter {
+public class tr_tl_conventer {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
